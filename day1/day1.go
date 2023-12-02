7fee56a0797fcdf0d6548c0ba1c8801a42988f3f
Marc <673790+MCunanan@users.noreply.github.com>
2023-12-01 22:04:55 -0500

    part 2 solution

--- day1/day1.go
+++ day1/day1.go
@@ -6,9 +6,39 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var alphas = regexp.MustCompile(`[a-zA-Z]+`)
+
+type NumericMapping struct {
+	Text  string
+	Digit string
+}
+
+var numericMap = map[int]NumericMapping{
+	1: {"one", "on1e"},
+	2: {"two", "tw2o"},
+	3: {"three", "thre3e"},
+	4: {"four", "4"},
+	5: {"five", "fiv5e"},
+	6: {"six", "6"},
+	7: {"seven", "seve7n"},
+	8: {"eight", "eigh8t"},
+	9: {"nine", "nin9e"},
+}
+
+func convertStringDigits(str string) string {
+	var keys []int
+	for k := range numericMap {
+		keys = append(keys, k)
+	}
+
+	for _, mapping := range numericMap {
+		str = strings.ReplaceAll(str, mapping.Text, mapping.Digit)
+	}
+	return str
+}
 
 func cleanString(str string) string {
 	return alphas.ReplaceAllString(str, "")
@@ -26,19 +56,21 @@ func SolvePuzzle(inputFile string) {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	sum, iter := 0, 1
+	sum, iter := 0, 0
 	for scanner.Scan() {
-		strNumber := cleanString(scanner.Text())
+		scanned := scanner.Text()
+		converted := convertStringDigits(scanned)
+		strNumber := cleanString(converted)
 
 		// take first and take last
 		trimmedNumber := string(strNumber[0]) + string(strNumber[len(strNumber)-1])
-		fmt.Printf("%d: %s\n", iter, trimmedNumber)
 		iter++
 
 		// convert
 		i, err := strconv.Atoi(trimmedNumber)
 		check(err)
 		sum += i
+		fmt.Printf("%d: %s - %s - %s - %s - %d\n", iter, scanned, converted, strNumber, trimmedNumber, sum)
 	}
 
 	fmt.Printf("Sum of Configuration Values: %d", sum)
